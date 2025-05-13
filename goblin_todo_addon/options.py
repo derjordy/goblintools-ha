ccfbad816f25a795fb089973fdf231b8374a84d8
derjordy <89782543+derjordy@users.noreply.github.com>
2025-05-13 12:55:11 +0200

    Delete goblin_todo_addon directory

--- goblin_todo_addon/options.py
+++
@@ -1,8 +0,0 @@
-# Optionen werden beim Start ins Python-Modul geschrieben
-import json
-
-with open('/data/options.json', 'r') as f:
-    opts = json.load(f)
-
-username = opts.get("username")
-password = opts.get("password")
